8d40d7def99b74b1d55407c469c018727a4fb925
Yash Raj <105196439+yashatyash@users.noreply.github.com>
2022-06-22 23:00:56 +0530

    Update Ques1.java

--- src/com/yash/java8assignments/Ques1/Ques1.java
+++ src/com/yash/java8assignments/Ques1/Ques1.java
@@ -4,5 +4,6 @@ public class Ques1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("GitHub");
+        System.out.println("Hi Github");
     }
 }
