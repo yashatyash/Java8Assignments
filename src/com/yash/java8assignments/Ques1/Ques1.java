d0eb490c3f25d1280f72767d9074046912bfe569
yashatyash <yash.raj@yash.com>
2022-06-22 22:54:21 +0530

    Commit on 22 June

--- src/com/yash/java8assignments/Ques1/Ques1.java
+++ src/com/yash/java8assignments/Ques1/Ques1.java
@@ -3,5 +3,6 @@ package com.yash.java8assignments.Ques1;
 public class Ques1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("GitHub");
     }
 }
