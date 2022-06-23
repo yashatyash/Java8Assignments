3acfd395fda02c19b5ba842786828b3d4f2b3a7b
yashatyash <yash.raj@yash.com>
2022-06-23 16:31:38 +0530

    Commit on 22 June

--- src/com/yash/java8assignments/Ques10/Ques10.java
+++ src/com/yash/java8assignments/Ques10/Ques10.java
@@ -2,6 +2,63 @@ package com.yash.java8assignments.Ques10;
 
 //WAP to create 10 objects of employee class, add all object to arraylist and from arraylist we need to convert this to map and set using stream api.
 
-public class Ques10 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+class Employee
+{
+    int empId;
+    String empName;
+
+    public Employee(int empId, String empName) {
+        this.empId = empId;
+        this.empName = empName;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+}
+
+public class Ques10 {
+    public static void main(String[] args) {
+
+        //creating list of employee
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee(1,"A"));
+        employeeList.add(new Employee(2,"B"));
+        employeeList.add(new Employee(3,"C"));
+        employeeList.add(new Employee(4,"D"));
+        employeeList.add(new Employee(5,"E"));
+        employeeList.add(new Employee(6,"F"));
+        employeeList.add(new Employee(7,"G"));
+        employeeList.add(new Employee(8,"H"));
+        employeeList.add(new Employee(9,"I"));
+        employeeList.add(new Employee(10,"J"));
+
+        //converting list to map
+        Map<Integer, String> employeeMap = employeeList.stream().collect(Collectors.toMap(Employee::getEmpId,
+                Employee::getEmpName));
+        System.out.println(employeeMap);
+
+        //converting list to set
+        Set<Employee> employeeSet = employeeList.stream().collect(Collectors.toSet());
+        for (Employee emp:employeeSet)
+            System.out.println("Emp id: "+emp.empId+" Emp name: "+emp.empName);
+    }
 }
